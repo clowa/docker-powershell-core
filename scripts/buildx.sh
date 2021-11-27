4e898661e780d4933b43cc536829f42af261fd88
Cedric Ahlers <cedric.ahlers@posteo.de>
2021-11-27 19:26:05 +0100

    add option to build only stable releases

--- scripts/buildx.sh
+++ scripts/buildx.sh
@@ -1,10 +1,11 @@
 #! /bin/bash
 
-usage="$(basename "$0") [-l | -a] -- program to build docker images of terraform for multiple platforms.
+usage="$(basename "$0") [-l | -a | -s] -- program to build docker images of terraform for multiple platforms.
 
 where:
     -l  only build docker image of latest GitHub release
-    -a  build docker image of all available GitHub releases"
+    -a  build docker image of all available GitHub releases
+    -s  build docker image only on stable GitHub release"
 
 # function joinByChar() {
 #   local IFS="$1"
@@ -32,7 +33,7 @@ if [[ -z ${DOCKER_IMAGE+set} ]]; then
 fi
 
 if [[ -z ${PLATFORMS+set} ]]; then
-  echo "Environment variable PLATFORMS not set. Run \"export PLATFORMS=\"linux/amd64,linux/arm64\""
+  echo "Environment variable PLATFORMS not set. Run \"export PLATFORMS=linux/amd64,linux/arm64"
   exit 2
 fi
 
@@ -46,7 +47,7 @@ if [[ -z ${REPOSITORY+set} ]]; then
   exit 2
 fi
 
-while getopts :hla: flag
+while getopts ":hlas" flag
 do
     case "${flag}" in
         h)
@@ -57,13 +58,12 @@ do
           ## Only fetch latest releases
           LATEST=true
           VERSIONS=($(curl --silent https://api.github.com/repos/${ORGANIZATION}/${REPOSITORY}/releases/latest | jq --raw-output '.name' | tr -d 'v' | sed -e 's/Release of PowerShell//'))
-          set -e
           ;;
         a)
           ## Get all GitHub releases
           VERSIONS=($(curl --silent https://api.github.com/repos/${ORGANIZATION}/${REPOSITORY}/releases | jq --raw-output '.[].name' | tr -d 'v' | sed -e 's/Release of PowerShell//'))
           ;;
-        *)
+        s)
           ## Filter out alpha and beta releases
           VERSIONS=($(curl --silent https://api.github.com/repos/${ORGANIZATION}/${REPOSITORY}/releases | jq --raw-output '.[]|select(.name|test("^v\\d*\\.\\d*\\.\\d*\\s.*$"))|.name' | tr -d 'v' | sed -e 's/Release of PowerShell//'))
           ;;
